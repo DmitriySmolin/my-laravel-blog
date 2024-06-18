a599757118ab8da9ccaa94f09bc55bb67b50e781
Dmitry <neox56@gmail.com>
2024-06-18 23:25:55 +0400

    init project

---
+++ database/migrations/2024_06_17_152451_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedSmallInteger('gender')->nullable();
+            $table->date('birthed_at')->index();
+            $table->string('avatar_path')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->string('first_name')->nullable();
+            $table->string('second_name')->nullable();
+            $table->string('third_name')->nullable();
+            $table->string('login')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
