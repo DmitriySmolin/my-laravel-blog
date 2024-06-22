2046892c84ae7043c5be2d62365691a0566b021a
Dmitry <neox56@gmail.com>
2024-06-22 18:30:31 +0400

    finish lesson 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return 'test';
+});
+
+Route::get('/posts/store', [PostController::class, 'store']);
+Route::get('/posts/index', [PostController::class, 'index']);
+Route::get('/posts/{post}/show', [PostController::class, 'show']);
+Route::get('/posts/{post}/update', [PostController::class, 'update']);
+Route::get('/posts/{post}/destroy', [PostController::class, 'destroy']);
